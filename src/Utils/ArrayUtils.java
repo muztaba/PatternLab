8b9f1cee7ab234366157f2f0bf75b4c70ab86b7b
muztaba <hasanat.muztaba@gmail.com>
2015-07-11 22:29:45 +0600

    some new static method add to the ArrayUtils class

--- src/Utils/ArrayUtils.java
+++ src/Utils/ArrayUtils.java
@@ -1,7 +1,9 @@
 package Utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by seal on 7/2/15.
@@ -12,4 +14,36 @@ public class ArrayUtils {
         for (int i : array) list.add(i);
         return list;
     }
+
+
+    public static int[] makeArray(int length, int fillVal) {
+        int[] array = new int[length];
+        Arrays.fill(array, fillVal);
+        return array;
+    }
+
+    public static int[] makeArray(int length,int from, int to, int fillVal) {
+        int[] array = new int[length];
+        Arrays.fill(array, from, to, fillVal);
+        return array;
+    }
+
+    public static int[] makeArray(int length,int from, int to, Random random) {
+        int[] array = new int[length];
+        for (int i = from; i < to; i++) {
+            array[i] = random.nextInt();
+        }
+        return array;
+    }
+
+    public static int[] makeArray(int length,int from, int to,
+                                  int lowerBound, int upperBound, Random random) {
+        int[] array = new int[length];
+        for (int i = from; i < to; i++) {
+            array[i] = random.nextInt(lowerBound + upperBound) - upperBound;
+        }
+        return array;
+    }
+
+
 }
